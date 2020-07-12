b208c2379bbb86546cfb6e02556b9e6ca149d556
BaecMari <53397420+BaecMari@users.noreply.github.com>
2020-07-12 16:21:03 +0900

    내 가지 테스트
    내 가지 테스트

--- app/src/main/java/com/example/banananote/MainActivity.java
+++ app/src/main/java/com/example/banananote/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //내 가지 테스트
     }
 }
